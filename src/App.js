283b573cf0a7fe581fe49dd8e6e0eda80dd4bea0
TriplehashRanchi <triplehash.business@gmail.com>
2024-09-14 13:54:47 +0530

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Signup from './components/Auth/Signup';
+import Signin from './components/Auth/Signin';
+import CreateLink from './components/Links/CreateLink';
+import LinkList from './components/Links/LinkList';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/create-link" element={<CreateLink />} />
+          <Route path="/links" element={<LinkList />} />
+          <Route path="/" element={<LinkList />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
